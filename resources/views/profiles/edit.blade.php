37335db35c8553ecda074ff8ab5cda05665b2bba
polodev <polodev10@gmail.com>
2020-07-11 13:42:58 +0600

    update profile show and edit blade file

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -44,7 +44,7 @@
             @enderror
         </div>
 
-        <div class="mb-6">
+        <div class="mb-6 ">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
                   for="avatar"
             >
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -6,17 +6,27 @@
               alt=""
               class="mb-2"
         >
+        @if( isset ( $user->avatar) )
+          <img src="{{ $user->avatar }}"
+               alt="{{ $user->name }}"
+               class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
+               style="left: 50%"
+               width="150"
+          >
+        @else
+          <img src="{{ asset('/images/default-avatar.jpeg') }}"
+               alt="Default Avatar"
+               class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
+               style="left: 50%"
+               width="150"
+          >    
+        @endif
 
-        <img src="{{ $user->avatar }}"
-             alt=""
-             class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
-             style="left: 50%"
-             width="150"
-        >
+        
     </div>
 
-    <div class="flex justify-between items-center mb-4">
-        <div>
+    <div class="flex justify-between items-center mb-6">
+        <div style="max-width: 270px">
             <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
             <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
         </div>
