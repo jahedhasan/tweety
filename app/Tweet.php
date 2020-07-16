17a8755fcd6be88bdbb0ebdee5bcc07483532e24
Jahed <jahed.cse1@gmail.com>
2020-07-16 11:12:51 +0600

    working on like dislike

--- app/Tweet.php
+++ app/Tweet.php
@@ -2,51 +2,20 @@
 
 namespace App;
 
-use App\Like;
-use App\User;
+// use App\Like;
+// use App\User;
+ use App\Likable;
 use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
+    use Likable;
+
     protected $guarded = [];
 
     public function user(){
       return $this->belongsTo(User::class);
     }
-
-    public function like($user = null ,$liked = true)
-    {
-      $this->likes()->updateOrCreate([
-        'user_id' => $user ? $user->id : auth()->id(),
-      ],
-      [
-        'liked' => $liked,
-      ]);
-    }
-
-    public function dislike($user =null)
-    {
-      return $this->like($user , false);   
-    }
-
-    public function isLikedBy(User $user)
-    {
-      return (bool) $user->likes
-              ->where('tweet_id' , $this->id)
-              ->where('liked', true)
-              ->count();
-    }
-
-    public function isDislikedBy(User $user)
-    {
-      return (bool) $user->likes
-              ->where('tweet_id' , $this->id)
-              ->where('liked', false)
-              ->count();
-    }
-
-    public function likes()
-    {
-      return $this->hasMany(Like::class);
-    }
 }
+
+
